9611d96e278bd9be20dcc93402bc9d07353282fb
Architect-Jian <architect_jian@aliyun.com>
2020-10-26 14:31:19 +0800

    add index and bootstrap

--- src/main/java/com/un/architect/community/controller/HelloController.java
+++ src/main/java/com/un/architect/community/controller/HelloController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/Hello")
-    public String Hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String Index(){
+        return "index";
 
     }
 
